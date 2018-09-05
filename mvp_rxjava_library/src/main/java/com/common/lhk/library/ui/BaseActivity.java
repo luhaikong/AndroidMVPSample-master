6f69709414b80d7719b76cc28db9a6a7eacc03c8
luhaikong <1031350293@qq.com>
2018-09-05 14:26:39 +0800

    增加软件更新权限校验，适配安卓6.0，7.0

--- mvp_rxjava_library/src/main/java/com/common/lhk/library/ui/BaseActivity.java
+++ mvp_rxjava_library/src/main/java/com/common/lhk/library/ui/BaseActivity.java
@@ -1,16 +1,26 @@
 package com.common.lhk.library.ui;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
+    private static final int Storage_Permission = 0x01;
+    private static final String[] str_Storage = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
     public Context mBaseContext;
     public Activity mActivity;
 
@@ -19,6 +29,59 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBaseContext = this;
         mActivity = this;
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermission(str_Storage);
+        }
+    }
+
+    public void requestPermission(String[] permissions) {
+        for (String s:permissions){
+            if (ContextCompat.checkSelfPermission(this, s) != PackageManager.PERMISSION_GRANTED) {
+                //询问用户是否拒绝过，如果没有，该方法返回false，如果被拒绝过，该方法返回true
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, s)) {
+                    // 用户拒绝过这个权限了，应该提示用户，为什么需要这个权限
+                    permissionDialogShow(s);
+                } else {
+                    // 申请授权。
+                    ActivityCompat.requestPermissions(this, permissions, Storage_Permission);
+                }
+            }
+        }
+    }
+
+    public void permissionDialogShow(final String permission){
+        new AlertDialog.Builder(this)
+                .setTitle("友好提醒")
+                .setMessage("没有存储权限将不能下载文件，请把存储权限赐给我吧！")
+                .setPositiveButton("赏你", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        // 用户同意继续申请
+                        ActivityCompat.requestPermissions(BaseActivity.this, new String[]{permission}, Storage_Permission);
+                    }
+                })
+                .setNegativeButton("不给", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        // 用户拒绝申请
+                        requestPermission(new String[]{permission});
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == Storage_Permission) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //权限被用户同意,做相应的事情
+            } else {
+                //权限被用户拒绝，做相应事情
+                requestPermission(permissions);
+            }
+        }
     }
 
     public void toastShow(int resId) {
