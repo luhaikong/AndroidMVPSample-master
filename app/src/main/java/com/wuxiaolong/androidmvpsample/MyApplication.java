eb98a27d0d5280241f3d9515a9008e83e3e4a602
luhaikong <1031350293@qq.com>
2019-03-12 14:41:00 +0800

    升级SDK，添加常用第三方库

--- app/src/main/java/com/wuxiaolong/androidmvpsample/MyApplication.java
+++ app/src/main/java/com/wuxiaolong/androidmvpsample/MyApplication.java
@@ -2,6 +2,7 @@ package com.wuxiaolong.androidmvpsample;
 
 import com.common.lhk.library.BaseApplication;
 import com.common.lhk.library.files.AppDataFile;
+import com.wuxiaolong.androidmvpsample.utils.LogUtil;
 
 /**
  * Created by MyPC on 2018/5/21.
@@ -12,7 +13,8 @@ public class MyApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        AppDataFile.init("mvpsample");
-        this.registerComponentCallbacks(new MemoryCallBack());
+        AppDataFile.init("mvpsample");//初始化SD卡文件存储目录
+        this.registerComponentCallbacks(new MemoryCallBack());//设置内存管理监听
+        LogUtil.initLogger();//初始化日志打印工具
     }
 }
---
+++ app/src/main/java/com/wuxiaolong/androidmvpsample/utils/LogUtil.java
@@ -0,0 +1,114 @@
+package com.wuxiaolong.androidmvpsample.utils;
+
+import android.support.annotation.Nullable;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.BuildConfig;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
+/**
+ * @author Administrator
+ * @date 2014-12-23
+ */
+public class LogUtil {
+
+    private static final String customTag = "-WZT-";
+
+    public static void initLogger() {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+                .methodCount(0)         // (Optional) How many method line to show. Default 2
+                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+                //.logStrategy(customLog) // (Optional) Changes the log strategy to print out. Default LogCat
+                .tag(customTag)   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build();
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy) {
+            @Override
+            public boolean isLoggable(int priority, @Nullable String tag) {
+                return BuildConfig.DEBUG;
+            }
+        });
+    }
+
+    public static void d(String message) {
+        Logger.d(message);
+    }
+
+    public static void i(String message) {
+        Logger.i(message);
+    }
+
+    public static void w(String message) {
+        Logger.w(message);
+    }
+
+    public static void e(String message, Throwable e) {
+        Logger.e(e, message);
+    }
+
+    public static void json(String json) {
+        Logger.json(json);
+    }
+
+    public static void xml(String xml) {
+        Logger.xml(xml);
+    }
+
+    /**
+     * 格式化json字符串
+     *
+     * @param jsonStr 需要格式化的json串
+     * @return 格式化后的json串
+     */
+    public static String formatJson(String jsonStr) {
+        if (null == jsonStr || "".equals(jsonStr)) return "";
+        StringBuilder sb = new StringBuilder();
+        char last = '\0';
+        char current = '\0';
+        int indent = 0;
+        for (int i = 0; i < jsonStr.length(); i++) {
+            last = current;
+            current = jsonStr.charAt(i);
+            //遇到{ [换行，且下一行缩进
+            switch (current) {
+                case '{':
+                case '[':
+                    sb.append(current);
+                    sb.append('\n');
+                    indent++;
+                    addIndentBlank(sb, indent);
+                    break;
+                //遇到} ]换行，当前行缩进
+                case '}':
+                case ']':
+                    sb.append('\n');
+                    indent--;
+                    addIndentBlank(sb, indent);
+                    sb.append(current);
+                    break;
+                //遇到,换行
+                case ',':
+                    sb.append(current);
+                    if (last != '\\') {
+                        sb.append('\n');
+                        addIndentBlank(sb, indent);
+                    }
+                    break;
+                default:
+                    sb.append(current);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 添加space
+     */
+    private static void addIndentBlank(StringBuilder sb, int indent) {
+        for (int i = 0; i < indent; i++) {
+            sb.append('\t');
+        }
+    }
+}
