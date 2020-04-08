c4bba5890188964380152063980f8e79e10b25e4
lihongkun <1031350293@qq.com>
2020-04-08 10:02:11 +0800

    增加应用 全国哀悼日 灰色工具类

--- mvp_rxjava_library/src/main/java/com/common/lhk/library/utils/MourningUtils.java
+++ mvp_rxjava_library/src/main/java/com/common/lhk/library/utils/MourningUtils.java
@@ -20,8 +20,15 @@ public class MourningUtils {
      */
     public static void startMouning(Activity activity){
         Paint paint = new Paint();
+        // 新建颜色矩阵，修改色调
         ColorMatrix cm = new ColorMatrix();
+        // 修改饱和度为0
         cm.setSaturation(0);
+
+        // ColorMatrixColorFilter：一个通过4*5的颜色矩阵来改变颜色的颜色过滤器
+        // LightingColorFilter：一个可以模拟简单的灯光效果的颜色过滤器
+        // PorterDuffColorFilter：一个可以使用单个颜色和指定Porter-Duff模式来对源像素进行染色颜色过滤器
+        // 为画笔 设置颜色过滤
         paint.setColorFilter(new ColorMatrixColorFilter(cm));
         activity.getWindow().getDecorView().setLayerType(View.LAYER_TYPE_HARDWARE, paint);
     }
