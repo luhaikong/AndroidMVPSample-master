bbacc7d90bea9c9375b98a0e45e7da68b473a82d
lihongkun <1031350293@qq.com>
2020-04-08 09:35:28 +0800

    增加应用 全国哀悼日 灰色工具类

---
+++ mvp_rxjava_library/src/main/java/com/common/lhk/library/utils/MourningUtils.java
@@ -0,0 +1,27 @@
+package com.common.lhk.library.utils;
+
+import android.app.Activity;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2020/4/8.
+ * 全国哀悼日 界面颜色变 灰
+ */
+
+public class MourningUtils {
+
+    /**
+     * 启动全国哀悼主题色
+     * @param activity
+     */
+    public static void startMouning(Activity activity){
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        activity.getWindow().getDecorView().setLayerType(View.LAYER_TYPE_HARDWARE, paint);
+    }
+}
